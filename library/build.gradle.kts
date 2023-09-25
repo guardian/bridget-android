35277393cf19cb53bd55af139887f7b9335833d8
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2023-09-25 14:37:34 +0100

    Copy in thrift dependencies

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,33 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
 
 dependencies {
+    api(libs.apache.thrift) {
+//        // These modules are excluded because a version of them is included in Android libraries.
+//        // If we don't explicitly exclude them in this way, the gradle will exclude them for us,
+//        // but this produces warnings.
+//        exclude(group = libs.commons.logging.get().group, module = libs.commons.logging.get().module.name)
+//        exclude(group = libs.apache.httpClient.get().group, module = libs.apache.httpClient.get().module.name)
+//        exclude(group = libs.apache.tomcat.annotations.get().group, module = libs.apache.tomcat.annotations.get().module.name)
+//        exclude(group = libs.apache.tomcat.embed.get().group, module = libs.apache.tomcat.embed.get().module.name)
+    }
     testImplementation(libs.junit)
 }
+
+//tasks.register<Exec>("generateThriftClasses") {
+//    executable = "thrift"
+//    args?.plusAssign(
+//        listOf(
+//            "-out",
+//            "src/main/java/",
+//            "-gen",
+//            "java:generated_annotations=undated",
+//            "src/main/thrift/native.thrift",
+//        ),
+//    )
+//}
+
+
+//thrift -out src/main/java/ -gen java:generated_annotations=undated library/src/main/thrift/native.thrift

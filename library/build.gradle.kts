92c4a91f92cbceeddf3d72f152ed1ddcee3c2b46
Aditya Bhaskar <79363218+ab-gnm@users.noreply.github.com>
2023-09-25 14:46:55 +0100

    Add dependencies required for using thrift generated code.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -13,21 +13,15 @@ dependencies {
 //        exclude(group = libs.apache.tomcat.annotations.get().group, module = libs.apache.tomcat.annotations.get().module.name)
 //        exclude(group = libs.apache.tomcat.embed.get().group, module = libs.apache.tomcat.embed.get().module.name)
     }
-    testImplementation(libs.junit)
+    // Unlike the 0.15 release, the 0.17 gradle release of libthrift's POM file is missing
+    // dependency information so we have to supply it here. Taken from
+    // https://github.com/apache/thrift/blob/master/lib/java/gradle/environment.gradle and
+    // https://github.com/apache/thrift/blob/f066d84ffb7fe2eb0dc3b7e7cd0d0417a1ae7963/lib/java/gradle.properties
+    // TODO when next increasing thrift version check if POM file includes its dependencies again,
+    //  then we won't have to include them below.
+    api(libs.javax.annotationApi)
+    api(libs.slf4j.api)
+    api(libs.apache.httpClient)
 }
 
-//tasks.register<Exec>("generateThriftClasses") {
-//    executable = "thrift"
-//    args?.plusAssign(
-//        listOf(
-//            "-out",
-//            "src/main/java/",
-//            "-gen",
-//            "java:generated_annotations=undated",
-//            "src/main/thrift/native.thrift",
-//        ),
-//    )
-//}
-
-
-//thrift -out src/main/java/ -gen java:generated_annotations=undated library/src/main/thrift/native.thrift
+//thrift -out library/src/main/java/ -gen java:generated_annotations=undated library/src/main/thrift/native.thrift
--- library/src/main/kotlin/com/theguardian/bridgetandroid/Version.kt
+++
@@ -1,3 +0,0 @@
-package com.theguardian.bridgetandroid
-
-//const val BRIDGET_VERSION = "2.0.0"

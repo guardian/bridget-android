55429f3f609f87ba08ac83b36ddd80c7370dc636
Francesca Hammond <francesca.hammond@guardian.co.uk>
2023-10-25 11:48:53 +0100

    Change credential-helper location

--- scripts/create_bridget_pr.sh
+++ scripts/create_bridget_pr.sh
@@ -6,8 +6,7 @@ BRANCH_NAME=update-bridget-classes
 CURRENT_VERSION="$(git describe --tags --abbrev=0)"
 
 # Commit & push new bridget jar
-export GUARDIANANDROID_PAT=$1
-git config --global credential.helper "/bin/bash /scripts/credential-helper.sh"
+git config --global credential.helper "/bin/bash ./scripts/credential-helper.sh"
 git config --global user.email "guardian.android@gmail.com"
 
 # Checkout app repo and create branch

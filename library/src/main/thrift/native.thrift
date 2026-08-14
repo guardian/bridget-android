06867a196b84027abd5bc204262b55a04a85aba1
root <>
2026-08-14 13:35:15 +0000

    Update Thrift generated classes v8.14.0-2026-08-14-1

--- library/src/main/thrift/native.thrift
+++ library/src/main/thrift/native.thrift
@@ -250,6 +250,10 @@ service Navigation {
     void openPrivacyPolicy()
 }
 
+struct MaybeEmail {
+  1: optional string emailAddress
+}
+
 /**
  * Service to manage requests from the weblayer related to newsletter subscriptions.
  * added  version 2.0.0
@@ -257,9 +261,11 @@ service Navigation {
  *  - requestSignUp: request to sign up to a newsletter using an email address entered by the user.
  * Returns `true` if the request was successful, `false` if it failed for any reason. Exceptions
  * thrown will be discarded.
+ * - getLoggedInUserEmail: request currently signed in users email address. Blank if not signed in.
  */
 service Newsletters {
-    bool requestSignUp(1: string emailAddress, 2:string newsletterIdentityName)
+    bool requestSignUp(1: string emailAddress, 2:string newsletterIdentityName),
+    MaybeEmail getLoggedInUserEmail()
 }
 
 service Interaction {
@@ -287,4 +293,5 @@ service LiveActivities {
     bool unfollow(1: string activityType, 2: string activityId)
     bool isFollowing(1: string activityType, 2: string activityId)
 }
-const string BRIDGET_VERSION = "v8.13.2"
+
+const string BRIDGET_VERSION = "v8.14.0-2026-08-14-1"
